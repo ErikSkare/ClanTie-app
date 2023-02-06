6c841772d4857146d51ef764cdeb988c289fddc3
ErikSkare <skareerik55@gmail.com>
2023-02-06 14:24:50 +0100

    Camera component implemented

---
+++ apps/mobile/src/components/Camera.tsx
@@ -0,0 +1,165 @@
+import {useState, useEffect, useRef} from "react";
+import {View, TouchableOpacity, Image, Platform} from "react-native";
+import {
+  Camera as ExpoCamera,
+  CameraType as ExpoCameraType,
+  CameraProps as ExpoCameraProps,
+  FlashMode as ExpoFlashMode,
+} from "expo-camera";
+import {manipulateAsync, FlipType} from "expo-image-manipulator";
+import {MaterialCommunityIcons, Ionicons, AntDesign} from "@expo/vector-icons";
+import Button from "./Button";
+
+interface CameraProps extends ExpoCameraProps {
+  onPermissionDenied?: () => Promise<void>;
+  onPictureSend?: (pictureUri: string) => Promise<void>;
+  containerClassName?: string;
+}
+
+function transformRatio(ratio: string | undefined) {
+  if (!ratio) return;
+  const splitted = ratio.split(":");
+  return (
+    Number.parseInt(splitted[1] as string) /
+    Number.parseInt(splitted[0] as string)
+  );
+}
+
+const Camera: React.FC<CameraProps> = ({
+  onPermissionDenied = () => undefined,
+  onPictureSend = () => undefined,
+  containerClassName = "",
+  ...props
+}) => {
+  const cameraRef = useRef<ExpoCamera>(null);
+  const [permission, requestPermission] = ExpoCamera.useCameraPermissions();
+
+  const [type, setType] = useState<ExpoCameraType>(ExpoCameraType.back);
+  const [flash, setFlash] = useState<ExpoFlashMode>(ExpoFlashMode.off);
+  const [ratio, setRatio] = useState<string>();
+  const [pictureUri, setPictureUri] = useState<string>();
+
+  useEffect(() => {
+    if (!permission || (!permission.granted && permission.canAskAgain))
+      requestPermission();
+    else if (!permission.granted) onPermissionDenied();
+  }, [permission, requestPermission]);
+
+  async function initializeRatio(preferred: number) {
+    if (Platform.OS !== "android") return;
+    if (!cameraRef.current) return;
+    if (ratio) return;
+
+    const supporteds = await cameraRef.current.getSupportedRatiosAsync();
+    let min = Infinity;
+    let minLocation: string | undefined = undefined;
+    for (const s of supporteds) {
+      const splitted = s.split(":");
+      const current =
+        Number.parseInt(splitted[0] as string) /
+        Number.parseInt(splitted[1] as string);
+      if (Math.abs(current - preferred) < min) {
+        min = Math.abs(current - preferred);
+        minLocation = s;
+      }
+    }
+    setRatio(minLocation);
+  }
+
+  function toggleType() {
+    setType((current) =>
+      current == ExpoCameraType.back
+        ? ExpoCameraType.front
+        : ExpoCameraType.back
+    );
+  }
+
+  function toggleFlash() {
+    setFlash((current) =>
+      current == ExpoFlashMode.on ? ExpoFlashMode.off : ExpoFlashMode.on
+    );
+  }
+
+  async function takePicture() {
+    if (!cameraRef.current) return;
+    await cameraRef.current.takePictureAsync({
+      onPictureSaved: async (pic) => {
+        let res = pic;
+        if (type == ExpoCameraType.front)
+          res = await manipulateAsync(res.uri, [
+            {rotate: 180},
+            {flip: FlipType.Vertical},
+          ]);
+        setPictureUri(res.uri);
+      },
+      skipProcessing: true,
+      base64: false,
+    });
+  }
+
+  function clearPicture() {
+    setPictureUri(undefined);
+  }
+
+  if (!permission || !permission.granted) return null;
+
+  return (
+    <View
+      className={`bg-slate-900 flex justify-center items-center ${containerClassName}`}
+    >
+      {pictureUri ? (
+        <View
+          className="relative w-full max-h-full flex items-center"
+          style={{aspectRatio: transformRatio(ratio)}}
+        >
+          <TouchableOpacity
+            className="absolute left-4 top-4 z-10"
+            onPress={clearPicture}
+          >
+            <AntDesign name="close" size={24} color="white" />
+          </TouchableOpacity>
+          <Image source={{uri: pictureUri}} className="w-full h-full" />
+          <Button
+            content="Elküldöm"
+            className="absolute bottom-4"
+            onPress={() => onPictureSend(pictureUri)}
+          ></Button>
+        </View>
+      ) : (
+        <ExpoCamera
+          ref={cameraRef}
+          type={type}
+          flashMode={flash}
+          onCameraReady={() => initializeRatio(16 / 9)}
+          ratio={ratio}
+          className="w-full max-h-full"
+          style={{aspectRatio: transformRatio(ratio)}}
+          {...props}
+        >
+          <View className="absolute bottom-0 inset-x-0 flex flex-row justify-center items-center gap-8 pb-4">
+            <TouchableOpacity onPress={toggleFlash}>
+              {flash === ExpoFlashMode.on ? (
+                <Ionicons name="flash-off" size={24} color="white" />
+              ) : (
+                <Ionicons name="flash" size={24} color="white" />
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="w-20 h-20 rounded-full border-[5px] border-white"
+              onPress={takePicture}
+            />
+            <TouchableOpacity onPress={toggleType}>
+              <MaterialCommunityIcons
+                name="rotate-3d-variant"
+                size={24}
+                color="white"
+              />
+            </TouchableOpacity>
+          </View>
+        </ExpoCamera>
+      )}
+    </View>
+  );
+};
+
+export default Camera;
