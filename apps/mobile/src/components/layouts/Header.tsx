4f02abb7c58122d4511628312073b95bb1a9dd05
ErikSkare <skareerik55@gmail.com>
2023-02-23 18:43:08 +0100

    added beta caption to header

--- apps/mobile/src/components/layouts/Header.tsx
+++ apps/mobile/src/components/layouts/Header.tsx
@@ -1,4 +1,11 @@
-import {View, Text, ViewProps, TouchableOpacity} from "react-native";
+import {
+  View,
+  Text,
+  ViewProps,
+  TouchableOpacity,
+  Alert,
+  Linking,
+} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
 import {useNavigation} from "@react-navigation/native";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
@@ -14,22 +21,54 @@ const Header: React.FC<ViewProps> = ({className = "", ...props}) => {
       className={`w-full p-4 flex flex-row justify-between items-center ${className}`}
       {...props}
     >
-      <TouchableOpacity onPress={() => navigation.navigate("Feed")}>
-        <View className="flex flex-row">
+      <View className="flex-row items-center">
+        <TouchableOpacity onPress={() => navigation.navigate("Feed")}>
+          <View className="flex flex-row">
+            <Text
+              className="text-slate-400 text-base"
+              style={{fontFamily: "Roboto_400Regular"}}
+            >
+              Clan
+            </Text>
+            <Text
+              className="text-slate-400 text-base"
+              style={{fontFamily: "Roboto_700Bold"}}
+            >
+              Tie
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="p-2 bg-blue-400 rounded ml-2"
+          onPress={() =>
+            Alert.alert(
+              "Az applikáció még béta állapotban van!",
+              "Minden jellegű észrevételt/hibabejelentést szívesen várok!",
+              [
+                {
+                  text: "Hibabejelentés",
+                  onPress: () =>
+                    Linking.openURL(
+                      "mailto:skareerik55@gmail.com?subject=ClanTie - Hibabejelentés"
+                    ),
+                },
+                {
+                  text: "Bezárás",
+                  style: "cancel",
+                },
+              ],
+              {cancelable: true}
+            )
+          }
+        >
           <Text
-            className="text-slate-400 text-base"
-            style={{fontFamily: "Roboto_400Regular"}}
-          >
-            Clan
-          </Text>
-          <Text
-            className="text-slate-400 text-base"
+            className="uppercase text-white text-xs"
             style={{fontFamily: "Roboto_700Bold"}}
           >
-            Tie
+            BETA
           </Text>
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+      </View>
       <View className="flex flex-row items-center">
         <NotificationBell className="mr-6" />
         <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
